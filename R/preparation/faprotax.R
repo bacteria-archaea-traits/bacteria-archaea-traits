96813841e330cc7053a3b0230ed53b3f51a3ed16
Joshua Madin <jmadin@hawaii.edu>
2020-01-23 14:31:32 -1000

    Updated faprotax with new column names: pathways and reference.

--- R/preparation/faprotax.R
+++ R/preparation/faprotax.R
@@ -12,28 +12,28 @@ for (i in 1:length(fap)) {
   if (grepl("^\\*.*$", fap[i])) {
     temp <- strsplit(fap[i], "\t.*\\# ")[[1]]
     species <- trim(gsub("\\*", " ", temp[1]))
-    ref <- temp[2]
-    store <- rbind(store, data.frame(species, group, subgroup=NA, ref, note))
+    reference <- temp[2]
+    store <- rbind(store, data.frame(species, pathways, subpathways=NA, reference, note))
   } else {
     if (grepl("^add_group.*$", fap[i])) {
       grp <- gsub("add_group:", "", fap[i])
       grp <- trim(strsplit(grp, "\t")[[1]][1])
       
-      chunk <- store[store$group==grp,]
-      chunk$subgroup <- chunk$group
-      chunk$group <- group
+      chunk <- store[store$pathways==grp,]
+      chunk$subpathways <- chunk$pathways
+      chunk$pathways <- pathways
         
       store <- rbind(store, chunk)
     } else {
       temp <- strsplit(fap[i], "\t")[[1]]
-      group <- trim(temp[1])
+      pathways <- trim(temp[1])
       note <- temp[2]
     }
   }
 }
 dim(store)
 
-store1 <- store[!duplicated(store[c("species", "group", "subgroup")]),]
+store1 <- store[!duplicated(store[c("species", "pathways", "subpathways")]),]
 dim(store1)
 
 store2 <- store1[store1$species %in% nam$name_txt,]
